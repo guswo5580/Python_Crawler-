9fe96f681fad837893e5925d770823b9b234b2ad
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-04-16 18:20:50 +0900

    making Json file

--- Naver_Shopping/stage_naver_shopping.py
+++ Naver_Shopping/stage_naver_shopping.py
@@ -1,6 +1,12 @@
 from libs.naver_shopping.crawler import crawl
 from libs.naver_shopping.parser import parse
+import json
 
 pageString = crawl('')
 products = parse(pageString)
 print(products)
+
+file = open('./products.json', 'w+')
+#JSON형식의 파일을 디렉토리 폴더 안에 생성 , w+ 붙여 줄것
+file.write(json.dumps(products))
+#생성된 JSON파일에 products의 내용 작성
