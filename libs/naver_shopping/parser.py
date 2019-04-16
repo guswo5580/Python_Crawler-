65e3affa1dfe3e06eaeec2a411a839397e87df66
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-04-16 18:10:48 +0900

    making crawl and parse libs

--- libs/naver_shopping/parser.py
+++ libs/naver_shopping/parser.py
@@ -1,8 +1,14 @@
 from bs4 import BeautifulSoup
 
 def getProductInfo(li) :
-    print(li)
-    return {}
+    img = li.find('img')
+    alt = img['alt']
+    #태그가 아닌 속성 접근 시 []를 이용
+    priceReload = li.find('span', {'class' : '_price_reload'})
+    aTit = li.find('a', {'class' : 'tit'})
+    href = aTit['href']
+    return { 'name' : alt , 'price' : priceReload.text.replace(',', '') ,
+             'link' : href }
 
 def parse(pageString) :
     bsObj = BeautifulSoup(pageString, "html.parser")
@@ -10,7 +16,7 @@ def parse(pageString) :
     lis = ul.findAll('li', {'class' : '_itemSection'})
 
     products = []
-    for li in lis :
+    for li in lis[:1] :
         product = getProductInfo(li)
         products.append(product)
     return products
